dde5980506bcdf96b4a7a0d174c883efc63626fe
junjongwook <junjongwook@naver.com>
2022-06-22 19:43:46 +0900

    first commit

--- src/main/java/temp.java
+++ src/main/java/temp.java
@@ -9,62 +9,14 @@ public class temp {
 
     public static void main(String[] args) {
 
-        List<Apple> inventory = Arrays.asList(
-                new Apple(80, "green"),
-                new Apple(155, "green"),
-                new Apple(120, "red")
-        );
+        Runnable r1 = () -> { System.out.println("Hello, Runnable"); };
+        process(r1);
 
-        List<Apple> result = filterApples(inventory, a -> a.getWeight() > 150);
-
-        System.out.println(result);
-
+        process(() -> System.out.println("Hello, ahahaha"));
     }
 
-    static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+    public static void process(Runnable r) {
 
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple : inventory) {
-
-            if (p.test(apple)) {
-
-                result.add(apple);
-            }
-        }
-
-        return result;
-    }
-    public static class Apple {
-
-        private int weight = 0;
-        private String color = "";
-
-        public Apple(int weight, String color) {
-            this.weight = weight;
-            this.color = color;
-        }
-
-        public int getWeight() {
-            return weight;
-        }
-
-        public void setWeight(int weight) {
-            this.weight = weight;
-        }
-
-        public String getColor() {
-            return color;
-        }
-
-        public void setColor(String color) {
-            this.color = color;
-        }
-
-        @SuppressWarnings("boxing")
-        @Override
-        public String toString() {
-            return String.format("Apple{color='%s', weight=%d}", color, weight);
-        }
-
+        r.run();
     }
 }
