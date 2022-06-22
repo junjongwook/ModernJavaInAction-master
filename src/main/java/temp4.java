8ab798c317d7bfee26a3bc24a4a73ca89cd4eeae
junjongwook <junjongwook@naver.com>
2022-06-23 01:54:13 +0900

    IntStream Test

---
+++ src/main/java/temp4.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class temp4 {
+
+    public static void main(String[] args) {
+
+        IntStream stream = IntStream.rangeClosed(1, 100);
+        int[] ints = stream.toArray();
+
+        System.out.println(Arrays.toString(ints));
+    }
+}
